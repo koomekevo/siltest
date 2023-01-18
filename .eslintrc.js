14a29542a3cc18d0b79d1545252b9675b08da917
koomekevo <koome.kevo@gurucrafts.net>
2023-01-19 00:24:43 +0300

    Fix linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 /* eslint-disable no-undef */
 module.exports = {
   "env": {
-      "browser": true,
-      "es2021": true
+    "browser": true,
+    "es2021": true
   },
-  "extends": "eslint:recommended",
+  "extends": 'eslint:recommended',
   "parserOptions": {
-      "ecmaFeatures": {
-        "jsx": true
-      },
-      "ecmaVersion": "latest",
-      "sourceType": "module"
-  },
-  "plugins": ["react"],
-  "rules": {
-      "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
-      "react/react-in-jsx-scope": "off",
-      "import/no-unresolved": "off",
-      "no-shadow": "off",
-      "no-unused-vars": "off",
-      "no-undef": "off"
+    "ecmaFeatures": {
+      "jsx": true
     },
+    "ecmaVersion": 'latest',
+    "sourceType": 'module'
+  },
+  "plugins": ['react'],
+  "rules": {
+    "react/jsx-filename-extension": ['warn', { "extensions": ['.js', '.jsx'] }],
+    "react/react-in-jsx-scope": 'off',
+    "import/no-unresolved": 'off',
+    "no-shadow": 'off',
+    "no-unused-vars": 'off',
+    "no-undef": 'off'
+  },
   "ignorePatterns": [
-      "dist/",
-      "build/",
-      ".eslintrc"
+    "dist/",
+    "build/",
   ]
 }
