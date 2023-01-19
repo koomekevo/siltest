cb672edc3ca80057561dc0ddbf78b2ac07c1c45d
koomekevo <koome.kevo@gurucrafts.net>
2023-01-20 00:02:17 +0300

    Remove <Header /> from App.js to Home.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
-import Header from './components/Header'
 import Home from './components/Home'
 
 function App () {
   return (
-    <div className='bg-cover bg-center bg-fixed h-screen w-full bg-[url("https://images.unsplash.com/photo-1502982720700-bfff97f2ecac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")]'>
-      <Header />
-      <Home />
+    <div>
+      <div className='bg-cover bg-center bg-fixed h-screen w-full bg-[url("https://images.unsplash.com/photo-1502982720700-bfff97f2ecac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")]'>
+        <Home />
+      </div>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,36 @@
 import React from 'react'
+import Header from './Header'
 
 const Home = () => {
   return (
-    <div className='flex justify-center items-center min-h-[80%]'>
-      <div>
-        <div className='flex text-6xl md:text-6xl font-bold text-white'>
-          <p>
-            Your Ultimate Photography Directory.
-            <br />
-          </p>
-        </div>
-        <div className='text-4xl text-white text-center'>
-          <p>
-            Manage your photo portfolio with our user friendly app.
-            <br /><br />
-          </p>
-        </div>
-        <div className='flex justify-center space-x-8'>
-          <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-96 px-4 rounded-full'>
-            SIGN IN
-          </button>
-          <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-96 px-4 rounded-full'>
-            GET IN TOUCH
-          </button>
+    <>
+      <Header />
+      <div className='flex justify-center items-center min-h-[80%]'>
+        <div>
+          <div className='flex text-6xl md:text-6xl font-bold text-white'>
+            <p>
+              Your Ultimate Photography Directory.
+              <br />
+            </p>
+          </div>
+          <div className='text-4xl text-white text-center'>
+            <p>
+              Manage your photo portfolio with our user friendly app.
+              <br />
+              <br />
+            </p>
+          </div>
+          <div className='flex justify-center space-x-8'>
+            <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-96 px-4 rounded-full'>
+              SIGN IN
+            </button>
+            <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-96 px-4 rounded-full'>
+              GET IN TOUCH
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
