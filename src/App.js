cf1017d3476e5ad677c22dfb54128ba11556dda9
koomekevo <koome.kevo@gurucrafts.net>
2023-01-19 18:40:24 +0300

    Change background image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home'
 
 function App () {
   return (
-    <div className='bg-cover bg-center bg-fixed h-screen w-full bg-[url("https://images.unsplash.com/photo-1576091160550-2173dba999ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")]'>
+    <div className='bg-cover bg-center bg-fixed h-screen w-full bg-[url("https://images.unsplash.com/photo-1502982720700-bfff97f2ecac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")]'>
       <Header />
       <Home />
     </div>
