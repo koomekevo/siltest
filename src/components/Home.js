6a94bfc0c23ea88962b5f3778787df5d5727c459
koomekevo <koome.kevo@gurucrafts.net>
2023-01-19 21:14:49 +0300

    Add Sign in and Get in Touch buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center items-center min-h-[80%]'>
       <div>
-        <div className='text-6xl md:text-6xl font-bold text-white'>
+        <div className='flex text-6xl md:text-6xl font-bold text-white'>
           <p>
-            Your Ultimate <br /> Photography <br /> Directory. <br />
+            Your Ultimate Photography Directory.
+            <br />
           </p>
         </div>
-        <div>
-          <p className='text-lg text-white'>
+        <div className='text-4xl text-white text-center'>
+          <p>
             Manage your photo portfolio with our user friendly app.
+            <br /><br />
           </p>
+        </div>
+        <div className='flex justify-center space-x-8'>
+          <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-96 px-4 rounded-full'>
+            SIGN IN
+          </button>
+          <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-96 px-4 rounded-full'>
+            GET IN TOUCH
+          </button>
         </div>
       </div>
     </div>
