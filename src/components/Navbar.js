8514edbbf6b3c91146f7303b5fdebff8c03e9971
koomekevo <koome.kevo@gurucrafts.net>
2023-01-19 14:58:06 +0300

    Add opacity of 80 in nav class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Logo from '../images/logo.png'
 
 const Navbar = () => {
   return (
-    <nav class='bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
+    <nav class='bg-white opacity-80 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
       <div class='container flex flex-wrap items-center justify-between mx-auto'>
         <a href='https://www.savannahinformatics.com/' class='flex items-center'>
           <img
