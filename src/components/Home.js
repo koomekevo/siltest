db40c2886cffc1c4d7f0b5c06861ca51f2d233b2
koomekevo <koome.kevo@gurucrafts.net>
2023-01-19 19:30:06 +0300

    Center content in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='grid grid-cols-2 justify-center items-center'>
-      <div className='text-6xl md:text-6xl font-bold text-white'>
-        <p>Your Ultimate <br /> Photography <br /> Directory</p>
+    <div className='flex justify-center'>
+      <div>
+        <div className='text-6xl md:text-6xl font-bold text-white'>
+          <p>
+            Your Ultimate <br /> Photography <br /> Directory. <br />
+          </p>
+        </div>
+        <div>
+          <p className='text-lg text-white'>
+            Manage your photo portfolio with our user friendly app.
+          </p>
+        </div>
       </div>
     </div>
   )
