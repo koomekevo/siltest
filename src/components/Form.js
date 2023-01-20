6e81f761edd394778ee86464346f363794f9395d
koomekevo <koome.kevo@gurucrafts.net>
2023-01-20 16:23:57 +0300

    Add Email and Password input on Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,28 @@ import React from 'react'
 const Form = () => {
   return (
     <div>
-      <h2>Form</h2>
+      <h1>Sign In</h1>
+      <p>Please enter your details.</p>
+      <div>
+        <div>
+          <label>Email</label>
+          <input className='' placeholder='Enter your Email' />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            className=''
+            placeholder='Enter your Password'
+            type='password'
+          />
+        </div>
+        <div>
+          <div>
+            <input type='checkbox' id='remember' />
+            <label for='remember'>Remember for 10 days</label>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
