d5007b73690881a6be135e6c44b80468c11e1045
koomekevo <koome.kevo@gurucrafts.net>
2023-01-20 13:44:58 +0300

    Add split screen to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from './Header'
+import Form from './Form'
 
 const Login = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className='flex w-full h-screen'>
+        <div className='w-full flex justify-center items-center lg:w-1/2'>
+          <Form />
+        </div>
+        <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200' />
+      </div>
+    </>
   )
 }
 
