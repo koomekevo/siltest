f43d969a25dafd8ace4b9f86f95834b24ac067a0
koomekevo <koome.kevo@gurucrafts.net>
2023-01-20 10:56:47 +0300

    Change nav bottom border color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import Logo from '../images/logo.png'
 
 const Navbar = () => {
   return (
-    <nav class='bg-white opacity-80 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
+    <nav class='bg-white opacity-80 border-black border-b-2 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
       <div class='container flex flex-wrap items-center justify-between mx-auto'>
         <a
-          href='https://www.savannahinformatics.com/'
+          href='/'
           class='flex items-center'
         >
           <img src={Logo} class='h-6 mr-3 sm:h-9' alt='SILTest Logo' />
