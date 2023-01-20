ccf5e84a1c8afb7ea6d471bc36963b1cd7fefb4f
koomekevo <koome.kevo@gurucrafts.net>
2023-01-20 17:30:55 +0300

    Add Form component styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
     <>
       <Header />
       <div className='flex w-full h-screen max-h-[36.5rem] border-black border-b-2'>
-        <div className='w-full flex justify-center items-center lg:w-1/2'>
+        <div className='w-full flex justify-center items-center lg:w-1/2 bg-gray-200'>
           <Form />
         </div>
         <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200 bg-[url("https://images.unsplash.com/photo-1452587925148-ce544e77e70d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")]' />
