fcf303d79f5ab6de1d06130825d7a1a402eb21ef
koomekevo <koome.kevo@gurucrafts.net>
2023-01-20 13:59:37 +0300

    Modify max-h in Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,11 +6,11 @@ const Login = () => {
   return (
     <>
       <Header />
-      <div className='flex w-full h-screen'>
+      <div className='flex w-full h-screen max-h-[36.5rem]'>
         <div className='w-full flex justify-center items-center lg:w-1/2'>
           <Form />
         </div>
-        <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200' />
+        <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200 bg-[url("https://images.unsplash.com/photo-1452587925148-ce544e77e70d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")]' />
       </div>
     </>
   )
