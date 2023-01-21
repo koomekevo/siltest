5ddba34564690ca649b5d85ce6f83da703fdc5b3
koomekevo <koome.kevo@gurucrafts.net>
2023-01-21 14:53:29 +0300

    Add Sign Up Button

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-100'>
-      <h1 className='text-4xl font-semibold'>Sign In</h1>
+    <div className='bg-white px-5 py-10 rounded-3xl border-2 border-gray-100'>
+      <h1 className='text-3xl font-semibold'>Sign In</h1>
       <p className='font-medium text-lg text-gray-500 mt-2'>
         Please enter your details.
       </p>
@@ -23,7 +23,9 @@ const Form = () => {
           />
         </div>
         <div className='mt-4 flex justify-between items-center'>
-          <button className='font-small text-violet'>Forgot Password</button>
+          <button className='font-small text-violet text-sm'>
+            Forgot Password
+          </button>
         </div>
         <div className='mt-8 flex flex-col gap-y-4'>
           <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out rounded-xl bg-blue-900 text-white text-lg font-bold'>
@@ -36,10 +38,16 @@ const Form = () => {
               width='25px'
               height='25px'
             >
-              <path d="M25.004,22.006l16.025,0.022C42.427,28.656,39.87,42,25.004,42C15.613,42,8,34.389,8,25S15.613,8,25.004,8	c4.411,0,8.428,1.679,11.45,4.432l-4.785,4.783c-1.794-1.536-4.118-2.47-6.665-2.47c-5.664,0-10.256,4.591-10.256,10.254	s4.592,10.254,10.256,10.254c4.757,0,8.046-2.816,9.256-6.752h-9.256V22.006z" />{/* eslint-disable-line */}
+              <path d='M25.004,22.006l16.025,0.022C42.427,28.656,39.87,42,25.004,42C15.613,42,8,34.389,8,25S15.613,8,25.004,8	c4.411,0,8.428,1.679,11.45,4.432l-4.785,4.783c-1.794-1.536-4.118-2.47-6.665-2.47c-5.664,0-10.256,4.591-10.256,10.254	s4.592,10.254,10.256,10.254c4.757,0,8.046-2.816,9.256-6.752h-9.256V22.006z' /> {/* eslint-disable-line */}
             </svg>
             SIGN IN WITH GOOGLE
           </button>
+        </div>
+        <div className='mt-8 flex justify-center items-center'>
+          <p className='font-medium text-base'>
+            Don't have an account?{' '}
+            <button className='text-blue-900'>Sign Up</button>
+          </p>
         </div>
       </div>
     </div>
