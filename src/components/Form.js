77aac60e7604ef5b2f18d8b7cbbe2c042ec09d89
koomekevo <koome.kevo@gurucrafts.net>
2023-01-22 14:49:12 +0300

    Implement GoogleOAuthProvider and useGoogleLogin

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useGoogleLogin } from '@react-oauth/google'
 
 const Form = () => {
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse)
+  })
+
   return (
     <div className='bg-white px-5 py-10 rounded-3xl border-2 border-gray-100'>
       <h1 className='text-3xl font-semibold'>Sign In</h1>
@@ -31,14 +36,17 @@ const Form = () => {
           <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out rounded-xl bg-blue-900 text-white text-lg font-bold'>
             SIGN IN
           </button>
-          <button className='flex border-2 border-gray-100 rounded-xl items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out'>
+          <button
+            className='flex border-2 border-gray-100 rounded-xl items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out'
+            onClick={() => login()}
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 50 50'
               width='25px'
               height='25px'
             >
-              <path d='M25.004,22.006l16.025,0.022C42.427,28.656,39.87,42,25.004,42C15.613,42,8,34.389,8,25S15.613,8,25.004,8	c4.411,0,8.428,1.679,11.45,4.432l-4.785,4.783c-1.794-1.536-4.118-2.47-6.665-2.47c-5.664,0-10.256,4.591-10.256,10.254	s4.592,10.254,10.256,10.254c4.757,0,8.046-2.816,9.256-6.752h-9.256V22.006z' /> {/* eslint-disable-line */}
+              <path d="M25.004,22.006l16.025,0.022C42.427,28.656,39.87,42,25.004,42C15.613,42,8,34.389,8,25S15.613,8,25.004,8	c4.411,0,8.428,1.679,11.45,4.432l-4.785,4.783c-1.794-1.536-4.118-2.47-6.665-2.47c-5.664,0-10.256,4.591-10.256,10.254	s4.592,10.254,10.256,10.254c4.757,0,8.046-2.816,9.256-6.752h-9.256V22.006z" />{/* eslint-disable-line */}
             </svg>
             SIGN IN WITH GOOGLE
           </button>
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+// 799264714097-dbodgnk76i1ce9rh9md2hi1cjhvoud1a.apps.googleusercontent.com
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='799264714097-dbodgnk76i1ce9rh9md2hi1cjhvoud1a.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
