d15fc2d946f51b0344f68727f7e6927e0051bf00
koomekevo <koome.kevo@gurucrafts.net>
2023-01-23 15:42:37 +0300

    Install firebase and add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAhDQbULv6THXODzmijQkgMXiTTJqfRSys',
+  authDomain: 'siltest.firebaseapp.com',
+  projectId: 'siltest',
+  storageBucket: 'siltest.appspot.com',
+  messagingSenderId: '799264714097',
+  appId: '1:799264714097:web:31b532b5091f992be75ad0'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig) //eslint-disable-line
