c929a33129b8c9cb0b784610846f7c2814221068
koomekevo <koome.kevo@gurucrafts.net>
2023-01-25 01:50:44 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import About from "./pages/About";
-import SignUp from "./pages/SignUp";
-import Account from "./pages/Account";
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Account from './pages/Account'
 
-function App() {
+function App () {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/account" element={<Account />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
