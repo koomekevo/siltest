50385c3fe42d245d04cf67159260bb730995d451
koomekevo <koome.kevo@gurucrafts.net>
2023-01-25 13:38:19 +0300

    Install Flowbite

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,7 @@ const Navbar = () => {
   return (
     <nav class='bg-white opacity-80 border-black border-b-2 px-2 sm:px-4 py-2.5 dark:bg-gray-900'>
       <div class='container flex flex-wrap items-center justify-between mx-auto'>
-        <a
-          href='/'
-          class='flex items-center'
-        >
+        <a href='/' class='flex items-center'>
           <img src={Logo} class='h-6 mr-3 sm:h-9' alt='SILTest Logo' />
           <span class='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
             SILTest
@@ -53,25 +50,22 @@ const Navbar = () => {
         >
           <ul class='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
-              <div
-                href='#'
+              <NavLink
+                to='/'
                 class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
-                aria-current='page'
+                exact
               >
-                <NavLink to='/' exact>
-                  Home
-                </NavLink>
-              </div>
+                Home
+              </NavLink>
             </li>
             <li>
-              <div
-                href='#'
+              <NavLink
+                to='/About'
                 class='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                exact
               >
-                <NavLink to='/About' exact>
-                  About
-                </NavLink>
-              </div>
+                About
+              </NavLink>
             </li>
           </ul>
         </div>
