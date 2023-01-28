5aafe2e8c9a37e0c22fa65cb73c160b0453a55ba
koomekevo <koome.kevo@gurucrafts.net>
2023-01-28 16:24:05 +0300

    Add Navbar.test.js

---
+++ src/__tests__/Navbar.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import TestRenderer from "react-test-renderer";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import React from "react";
+
+describe("Render the Navbar", () => {
+  it("Renders the Navbar", () => {
+    const navbar = TestRenderer.create(
+      <Router>
+        <Navbar />
+      </Router>
+    ).toJSON();
+    expect(navbar).toMatchSnapshot();
+  });
+});
